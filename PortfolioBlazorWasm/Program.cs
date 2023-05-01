9b41be8604a7eab82007ef63467f1f900a5519af
AbiRaiCs <raiabishekcs@gmail.com>
2023-05-01 15:33:50 +0100

    change pathfinding service to transient

--- PortfolioBlazorWasm/Program.cs
+++ PortfolioBlazorWasm/Program.cs
@@ -21,6 +21,7 @@ namespace PortfolioBlazorWasm
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+        builder.Services.AddTransient<IPathfindingRunner, PathfindingRunner>();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IValidator<ChangeSettings>, SettingModelFluentValidator>();
             builder.Services.AddScoped<IPathfindingRunner, PathfindingRunner>();
